01f37acf63f5ab7c19137ee8bbc29845ae59c226
Trang Ha Viet <viettrangha@gmail.com>
2018-06-27 21:07:13 +0700

    update App base component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
 import Counter from "./components/Counter";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/form/Login/Login.js';
+import Register from './components/form/Register/Register.js';
 
-class App extends Component {
-  render() {
-    return (
-      <Counter />
-    );
-  }
-}
+// @TODO: Why it not render Login when navigate to '/dang-nhap' Nga?
+const App = () => (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" render={() => (<h1>Home</h1>)} />
+        <Route exact path="/dang-nhap" component={Login} />
+        <Route exact path="/dang-ky-tai-khoan" component={Register} />
+        {/* <Counter /> */}
+        {/* <Route exact path="/danh-sach-khoa-hoc" component={} />
+        <Route exact path="/de-thi" component={} />
+        <Route exact path="/bai-viet" component={} />
+        <Route exact path="/nap-the" component={} />
+        <Route exact path="/thong-tin-ca-nhan/:UserName" component={} />
+        <Route exact path="/khoa-hoc/:CourseID/:CourseName" component={} />  */}
+
+      </Switch>
+    </div>
+  </Router>
+)
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,26 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
-
 import App from "./App";
 import store from "./store";
-
+// File luu router va tao store
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+// import {Router, Route, Switch} from 'react-router-dom';
+// import { ThroughProvider } from 'react-through';
+import { BrowserRouter } from 'react-router-dom';
 // By using <Provider />, the store will be made available for all the components in your application.
-
 render(
   <Provider store={store}>
-    <App />
+   <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>, document.getElementById("root")
 );
+// render((
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>), document.getElementById("root")
+// );
+
+// registerServiceWorker();
