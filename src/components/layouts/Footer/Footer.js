431cb9295f6c6790b2ed1ea1b727c1bfea4554c6
Trang Ha Viet <viettrangha@gmail.com>
2018-06-27 21:05:40 +0700

    change footer style to scss

--- src/components/layouts/Footer/Footer.js
+++ src/components/layouts/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import './Footer.css';
+import './Footer.scss';
 
 export default class Footer extends React.Component {
   render() {
